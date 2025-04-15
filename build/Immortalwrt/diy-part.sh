04aade740e0a8ebc5645bbaee8a20c458d132ead
xztxy <31698398+xztxy@users.noreply.github.com>
2025-04-15 08:32:56 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -121,8 +121,8 @@ function git_sparse_clone() {
 
 ##### Themes
 # 拉取argon主题
-git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+#git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 # 拉取酷猫主题
 git clone --depth=1 -b js https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
 git clone --depth=1 -b main https://github.com/sirpdboy/luci-app-kucat-config package/luci-app-kucat-config
